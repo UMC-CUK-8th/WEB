638ec33bb2c1a1a76561e779e44beca3dfdcab71
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-04-06 14:35:48 +0900

    feat/week3-mission1

--- Min/Movie/src/App.tsx
+++ Min/Movie/src/App.tsx
@@ -1,14 +1,16 @@
 // import UseEffectCounterPage from './02-useEffect/UseEffectCounterPage';
-import UseEffectError from './02-useEffect/UseEffectError';
+// import UseEffectError from './02-useEffect/UseEffectError';
 // import UseEffectPage from './02-useEffect/UseEffectPage';
 import './App.css';
+import MoviePage from './pages/MoviePage';
 
 export default function App() {
   return (
     <>
       {/* <UseEffectPage /> */}
       {/* <UseEffectCounterPage /> */}
-      <UseEffectError />
+      {/* <UseEffectError /> */}
+      <MoviePage />
     </>
   )
 }
