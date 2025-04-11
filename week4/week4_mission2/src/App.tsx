7e511c67c05ee7a0d0f6e22368cabad325f95bb3
shooukie <bigcape@naver.com>
2025-04-11 21:22:22 +0900

    feat: 4주차 미션2 추가

---
+++ week4/week4_mission2/src/App.tsx
@@ -0,0 +1,28 @@
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css'
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import HomeLayout from './layouts/HomeLayout';
+import SignupPage from './pages/SignupPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />, //공유하는 부분만! 
+    errorElement: <NotFoundPage />,
+    children: [
+      {index:true, element: <HomePage/ >},
+      {path:'login', element: <LoginPage/ >},
+      {path:'signup', element: <SignupPage/ >}
+    ]
+  }, 
+]);
+function App() {
+  
+
+  return <RouterProvider router={router} />;
+}
+
+export default App
