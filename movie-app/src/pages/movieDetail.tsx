e0e08f61a574075d2b2b5a5de1cb9051aa6c2872
kangdani04 <ksnowflower@naver.com>
2025-04-12 16:59:37 +0900

    feat: 3주차 추가 미션

--- movie-app/src/pages/movieDetail.tsx
+++ movie-app/src/pages/movieDetail.tsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Movie } from "../types/movie";
 import LoadingSpinner from "../components/loading";
+import defaultImage from "../assets/icon/defaultImage.png";
+import StarRating from "../components/starRating";
+import Trailer from "../components/trailer";
 
 type Cast = {
     id: number;
@@ -18,9 +21,17 @@ const MovieDetailPage = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [cast, setCast] = useState<Cast[]>([]);
+    const [videoKey, setVideoKey] = useState<string | null>(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const API_URL = import.meta.env.VITE_TMDB_MOVIE_API_URL;
     const ACCESS_TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+
+    const openTrailer = () => {
+        if (videoKey) {
+            setIsModalOpen(true);
+        }
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -45,7 +56,17 @@ const MovieDetailPage = () => {
                 },
                 }
             );
-    
+
+            const videoRes = await axios.get(`${API_URL}/movie/${movieId}/videos?language=ko-KR`, {
+                headers: {
+                  Authorization: `Bearer ${ACCESS_TOKEN}`,
+                },
+            });
+            const trailer = videoRes.data.results.find(
+                (v: any) => v.type === "Trailer" && v.site === "YouTube"
+            );
+            setVideoKey(trailer?.key || null);
+            
             const directors = creditRes.data.crew.filter((person) => person.job === "Director");
             const actors = creditRes.data.cast.slice(0, 10); // 상위 10명만
     
@@ -86,19 +107,35 @@ const MovieDetailPage = () => {
                 className="w-full md:w-64 rounded shadow"
                 />
                 <div className="flex flex-col justify-between">
-                    <h1 className="text-3xl font-bold mb-4">{movie.title}</h1>
-                    <p className="text-sm text-gray-700 mb-1">⭐ 평점: {movie.vote_average}</p>
-                    <p className="text-sm text-gray-700 mb-1">📅 개봉일: {movie.release_date}</p>
-                    <p className="text-sm text-gray-700 mb-1">
-                    🎬 장르:{" "}
-                    {movie.genres?.map((genre) => genre.name).join(", ") || "정보 없음"}
+                    <h1 className="text-3xl font-bold mb-4">
+                        {movie.title}
+                        {videoKey && (
+                            <button
+                                onClick={openTrailer}
+                                className="text-3xl"
+                            >
+                                ▶️
+                            </button>
+                        )}
+                    </h1>
+                    <p className="text-sm text-gray-700 mb-1 flex items-center gap-2">
+                        <span className="font-semibold">평점 |</span>
+                        <StarRating rating={movie.vote_average} />
                     </p>
                     <p className="text-sm text-gray-700 mb-1">
-                    🌍 제작 국가:{" "}
-                    {movie.production_countries?.map((c) => c.name).join(", ") || "정보 없음"}
+                        <span className="font-semibold">개봉일 |</span> {movie.release_date}
+                    </p>
+                    <p className="text-sm text-gray-700 mb-1">
+                        <span className="font-semibold">장르 |</span>{" "}
+                        {movie.genres?.map((genre) => genre.name).join(", ") || "정보 없음"}
+                    </p>
+                    <p className="text-sm text-gray-700 mb-1">
+                        <span className="font-semibold">제작 국가 |</span>{" "}
+                        {movie.production_countries?.map((c) => c.name).join(", ") || "정보 없음"}
                     </p>
                     <p className="text-sm text-gray-700 mb-4">
-                    ⏱️ 런타임: {movie.runtime ? `${movie.runtime}분` : "정보 없음"}
+                        <span className="font-semibold">런타임 |</span>{" "}
+                        {movie.runtime ? `${movie.runtime}분` : "정보 없음"}
                     </p>
                     <p className="mt-2 text-sm text-gray-800 leading-relaxed">{movie.overview}</p>
                 </div>
@@ -108,21 +145,26 @@ const MovieDetailPage = () => {
                 <div className="grid grid-cols-5 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10 gap-3">
                     {cast.map((person) => (
                         <div key={person.id} className="p-2 flex flex-col items-center text-center w-[75px]">
-                        <img
-                            src={
-                            person.profile_path
-                                ? `https://image.tmdb.org/t/p/w185${person.profile_path}`
-                                : "https://via.placeholder.com/185x278?text=No+Image"
-                            }
-                            alt={person.name}
-                            className="w-full h-auto rounded mb-2 object-cover"
-                        />
-                        <p className="text-xs text-black-600 font-semibold mb-1">{person.character || person.job}</p>
-                        <p className="text-sm text-gray-800">{person.name}</p>
+                            <div className="w-full aspect-[185/278] overflow-hidden rounded mb-2 bg-gray-200 flex justify-center">
+                                <img
+                                    src={
+                                    person.profile_path
+                                        ? `https://image.tmdb.org/t/p/w185${person.profile_path}`
+                                        : defaultImage
+                                    }
+                                    alt={person.name}
+                                    className="w-full h-full rounded mb-2 object-cover"
+                                />
+                            </div>
+                            <p className="text-xs text-black-600 font-semibold mb-1">{person.character || person.job}</p>
+                            <p className="text-sm text-gray-800">{person.name}</p>
                         </div>
                     ))}
                 </div>
             </div>
+            {isModalOpen && videoKey && (
+                <Trailer videoKey={videoKey} onClose={() => setIsModalOpen(false)} />
+            )}
         </div>
     );
 };
