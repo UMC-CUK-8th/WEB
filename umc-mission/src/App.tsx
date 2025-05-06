cd4b51e7209ff9c000175c5e165bda81af109aa8
이현경 <leehyunkyung@ihyeongyeong-ui-MacBookAir.local>
2025-05-06 11:27:54 +0900

    feat : 6주차 미션 추가

--- umc-mission/src/App.tsx
+++ umc-mission/src/App.tsx
@@ -9,6 +9,8 @@ import MyPage from './pages/MyPage';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedLayout from './layouts/ProtectedLayout';
 import GoogleLoginRedirectPage from './pages/GoogleLoginRedirectPage';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 
 
@@ -43,15 +45,26 @@ const protectedRoutes:RouteObject[]=[
 
 const router= createBrowserRouter([...publicRoutes,...protectedRoutes]);
 
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+    },
+  },
+});
+
 function App() {
 
 
   return (
+    <QueryClientProvider client={queryClient}>
   <AuthProvider>
     <RouterProvider router={router}/>;
   </AuthProvider>
+  {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+  </QueryClientProvider>
   
 
 )}
 
-export default App
+export default App;
---
+++ umc-mission/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer= () =>{
+ return(
+    <footer className="bg-gray-100 dark:bg-gary-900 py-6 mt-12">
+        <div className="container mx-auto text-center text-gray-600 dark:text-gray-400">
+            <p>
+                &copy;{new Date().getFullYear()} 돌려돌려돌림판.All rights reserved.
+            </p>
+            <div className={'flex justify-cneter space-x-4 mt-4'}>
+                
+            </div>
+        </div>
+    </footer>
+ )
+}
+
+export default Footer;
